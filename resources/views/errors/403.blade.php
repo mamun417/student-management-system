dd1cf1423afca4b68b4c646517bca79766e33e79
fictionsoft50@gmail.com <Fsakabaka002>
2019-05-28 20:35:18 +0600

    403, 404 exception handle

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="ibox float-e-margins text-center">
+                <div class="ibox-content">
+
+                    <div class="table-responsive">
+                        <h1>403</h1>
+                        <h3 class="font-bold">Not Allow to Access</h3>
+
+                        <div class="error-desc">
+                            Sorry, Permission is denied.
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection()
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="middle-box text-center animated fadeInDown">
+
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="ibox float-e-margins text-center">
+                <div class="ibox-content">
+
+                    <div class="table-responsive">
+                        <h1>404</h1>
+                        <h3 class="font-bold">Page Not Found</h3>
+
+                        <div class="error-desc">
+                            Sorry, but the page you are looking for has note been found. Try checking the URL for error, then hit the refresh button on your browser or try found something else in our app.
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection()
